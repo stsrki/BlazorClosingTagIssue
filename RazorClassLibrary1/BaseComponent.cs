b637e7806c5496738cd635d5cff6b5dbfa9d9d60
Mladen Macanovic <mladen.macanovic@gmail.com>
2020-02-04 22:22:32 +0100

    Add project files.

---
+++ RazorClassLibrary1/BaseComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace RazorClassLibrary1
+{
+    public abstract class BaseComponent : ComponentBase
+    {
+    }
+}
---
+++ RazorClassLibrary1/Link.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Components;
+
+namespace RazorClassLibrary1
+{
+    public partial class Link : BaseComponent
+    {
+        [Parameter] public string Class { get; set; }
+
+        [Parameter] public string To { get; set; }
+
+        [Parameter] public string Title { get; set; }
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
